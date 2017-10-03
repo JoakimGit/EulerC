590c2284b796c9e152b933a2c3b9c7449e0a7565
Golden <joagyl@student.chalmers.se>
2017-10-03 23:53:50 +0200

    Solved E6: Sum square difference

--- E6.cpp
+++ E6.cpp
@@ -1,14 +1,35 @@
 /*
- * Project Euler problem 4
+ * Project Euler problem 6
  */
 
 #include <stdio.h>
 
 /* Function declaration */
-
+int sumSqDiff(int);
 
 /* Main */
 int main(void)
 {
-	return 0;
+	int num = 100;
+	int res = sumSqDiff(num);
+	printf("Sum square diff of first %d numbers is %d\n", num, res);
+}
+
+/*
+ *	Decription:
+ *		Finds difference between sum of the squares and square of the sum
+ *		of the first n numbers
+ */
+int sumSqDiff(int n)
+{
+	int current = 1;
+	int sqSum = 0;
+	int sumSq = 0;
+	while ( current <= n ) {
+		sqSum += current;
+		sumSq += current*current;
+		current ++;
+	}
+	int res = (sqSum*sqSum)-sumSq;
+	return res;
 }
