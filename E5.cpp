3f391a8182be85657865d1a29808f28161c2de4a
Golden <joagyl@student.chalmers.se>
2017-10-03 23:44:00 +0200

    Finished E5: Smallest multiple

--- E5.cpp
+++ E5.cpp
@@ -1,14 +1,39 @@
 /*
- * Project Euler problem 4
+ * Project Euler problem 5
  */
 
 #include <stdio.h>
 
 /* Function declaration */
-
+int evenlyDiv(void);
 
 /* Main */
 int main(void)
 {
+	int res = evenlyDiv();
+	printf("Smallest number is: %d\n", res);
+}
+
+/*
+ *
+ * Decription: return smallest positive number that is evenly divisable by
+ *			   numbers from 1 to 20.
+ *
+ */
+int evenlyDiv(void)
+{
+	int current = 1;
+	int done = 0;
+	int evenDiv;
+
+	while ( !done ) {
+		for ( int i=1; i<=20; i++ ) {
+			if ( current%i != 0 ) {
+				current++;
+				break;
+			}
+			if ( i==20 ) return current;
+		}
+	}
 	return 0;
 }
